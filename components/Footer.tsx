9c4685d9ef83c478114cef722ebcae8376b7f941
Azwar <azwarnaim97@gmail.com>
2023-05-20 21:42:08 +0800

    @AddImageAlt

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,11 +1,13 @@
+import Image from "next/image";
 import React from "react";
 
 function Footer() {
   return (
+    
     <div className="  bg-[#170f0f]">
       <div className="max-w-6xl m-auto">
         <div className=" text-gray-500 flex items-center">
-          <img src={"/azwarapp.png"} width={140} height={100} />
+          <Image src={"/azwarapp.png"} width={140} height={100} alt="Image"/>
           <span className="text-4xl font-light pr-3">|</span>
           <span className="pt-2">AZWAR APP @ ALL RIGHTS RESERVED</span>
         </div>
